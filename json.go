532b96e629b67abf7ee7c38d4a36fad7449e99f2
Max Thomas <max@weareunrivaled.com>
2026-05-07 13:47:34 -0600

    f

--- json.go
+++ json.go
@@ -29,9 +29,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		w.WriteHeader(500)
 		return
 	}
-	else{
-		log
-	}
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
